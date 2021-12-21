c2de1a9ed0ed18cbbb904176784a5cce53c67638
Troy Ballinger <troy@shippabo.com>
2021-12-21 03:20:02 -0800

    fix dependencies

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 // Bring in env variables
-require('dotenv').config();
+require("dotenv").config();
 
-const process = require('process');
-const controller = require('./controller.js');
+const process = require("process");
+const controller = require("./controller.js");
 
 // Setup motion sensor
-const gpio = require('onoff').Gpio;
-const pir = new gpio(4, 'in', 'both');
+const gpio = require("onoff").Gpio;
+const pir = new gpio(4, "in", "both");
 
 // Init servo
 controller.initializeServo();
 
 // Watch for motion
 pir.watch(async (err, value) => {
-	if (err) {
-		console.log("ERROR: Motion detector");
-	} else if (value == 1) { 
-		// Motion detected
-		await controller.sendPicToCloud();
-	} else {
-		// No motion
-	}
+  if (err) {
+    console.log("ERROR: Motion detector");
+  } else if (value == 1) {
+    // Motion detected
+    await controller.sendPicToCloud();
+  } else {
+    // No motion
+  }
 });
 
-console.log(`LicensePlateServer started successfully.`);
+console.log("LicensePlateServer started successfully.");
 
-process.on('exit', (code) => {
-	console.log(`LicensePlateServer exited with code ${code}.`);
+process.on("exit", (code) => {
+  console.log(`LicensePlateServer exited with code ${code}.`);
 });
--- controller.js
+++ controller.js
@@ -1,26 +1,39 @@
-const fs = require('fs');
-const request = require('request');
-const secrets = require('./creds/secrets.json');
-const admin = require('firebase-admin');
-const PiCamera = require('pi-camera');
+const axios = require("axios");
+const admin = require("firebase-admin");
+const fs = require("fs");
+const PiCamera = require("pi-camera");
+const s_gpio = require("pigpio").Gpio;
 
-// Time to keep the door open in seconds
-const SECONDS_OPEN = 10;
+const secrets = require("./creds/secrets.json");
 
-// Configure servo
-const s_gpio = require('pigpio').Gpio;
-const motor = new s_gpio(3, {mode: s_gpio.OUTPUT});
-const CLOSED_PW = 1500;
-const OPEN_PW = 850;
-let increment = 50;
-let current_pw;
+if (
+  !secrets ||
+  !secrets.database_url ||
+  !secrets.storage_bucket ||
+  !secrets.openalpr_key
+) {
+  console.err("Missing secrets.");
+  return;
+}
 
 // Firebase init
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-  databaseURL: "https://licenseplatesensor-10da0.firebaseio.com",
-  storageBucket: "licenseplatesensor-10da0.appspot.com"
+  databaseURL: secrets.database_url,
+  storageBucket: secrets.storage_bucket,
 });
+
+// Time to keep the door open in seconds
+const SECONDS_OPEN = 10;
+
+const MIN_CAR_CONFIDENCE = 75;
+
+// Configure servo
+const motor = new s_gpio(3, { mode: s_gpio.OUTPUT });
+const CLOSED_PW = 1500;
+const OPEN_PW = 850;
+let increment = 50;
+let current_pw;
 
 // Setup database and storage
 const database = admin.database();
@@ -28,269 +41,281 @@ const bucket = admin.storage().bucket();
 
 // Configure camera
 const myCamera = new PiCamera({
-	mode: 'photo',
-	output: `latest.jpg`,
-	width: 640,
-	height: 480,
-	nopreview: true,
+  mode: "photo",
+  output: `latest.jpg`,
+  width: 640,
+  height: 480,
+  nopreview: true,
 });
 
 // Handle interupts gracefully
-process.on('SIGINT', () => {
-	motor.servoWrite(0);
-	process.exit(0);
+process.on("SIGINT", () => {
+  motor.servoWrite(0);
+  process.exit(0);
 });
 
 // To check if door is already being opened
-var door_busy = false;
-var door_open = false;
-var processing_image = false;
+let door_busy = false;
+let door_open = false;
+let processing_image = false;
 
-// Actively look for acceptance of new cars into db, aka deletion from pending requests queue
-var ref = database.ref("LoadImage");
-ref.on("child_removed", function(snapshot) {
-	console.log(JSON.stringify(snapshot));
-	tryOpenDoor();
-}, function (errorObject) {
-	console.log("ERROR: Firebase (update) " + errorObject.code);
-});
+// Deletion from pending requests queue = acceptance of a new car
+const ref = database.ref("LoadImage");
+ref.on(
+  "child_removed",
+  (snapshot) => {
+    console.log(JSON.stringify(snapshot));
+    tryOpenDoor();
+  },
+  (error) => {
+    console.err("ERROR: Firebase", error.code);
+  }
+);
 
 // Setup openALPR request
-var url = "https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=" + secrets.openalpr_key;
+const url = `https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=${secrets.openalpr_key}`;
 
 /**
- *  Attempt to send a recently taken picture to be analyzed by OpenALPR via their API 
- *  @returns void
- */ 
-async function sendPicture() {
-	// Convert image to base64 for http request
-	var bitmap = fs.readFileSync('latest.jpg');
-	var base64string = new Buffer(bitmap).toString('base64');
-	var cars = undefined;
-	if (process.env.OPEN_ALPR) {
-		request.post({url: url, 
-			body: JSON.stringify(base64string)}, 
-			async (err, _httpResponse, body) => {
-				if (err) {
-					// Failure with error
-					console.log('upload failed:', err);
-					processing_image = false;
-					return;
-				}
-				if (!body) {
-					// Failure without error
-					processing_image = false;
-					return;
-				}
-				console.log('Upload complete!');
-				const results = JSON.parse(body).results;
-				// If OpenALPR has potential matches
-				if (results && results.length > 0 && results[0]['candidates'].length > 0) {
-					cars = [];
-					// Find those with 75% or greater confidence
-					results[0]['candidates'].forEach(car => {
-						if (car['confidence'] > 75) {
-							console.log(car);
-							var carEntry = {};
-							carEntry['plate'] = car['plate'];
-							carEntry['color'] = results[0]['vehicle']['color'][0]['name'];
-							carEntry['make'] = results[0]['vehicle']['make'][0]['name'];
-							carEntry['model'] = results[0]['vehicle']['make_model'][0]['name'];
-							cars.push(carEntry);
-						}
-					});
-				} else {
-					console.log('No results from OpenALPR');
-				}
-				await processResults(cars);
-		});
-	} else {
-		console.log('OpenALPR disabled, aborting.');
-		processing_image = false;
-	}
-}
+ *  Attempt to send a recently taken picture to be analyzed by OpenALPR via their API
+ */
+const sendPicture = async () => {
+  // Convert image to base64 for http request
+  const bitmap = fs.readFileSync("latest.jpg");
+  const base64string = new Buffer(bitmap).toString("base64");
+  let cars = undefined;
+
+  if (!process.env.OPEN_ALPR) {
+    console.err("OpenALPR disabled.");
+    processing_image = false;
+    return;
+  }
+
+  axios
+    .post(url, JSON.stringify(base64string))
+    .then((response) => {
+      if (!body) {
+        processing_image = false;
+        return;
+      }
+      console.log("Upload complete!");
+      const results = JSON.parse(response.body).results;
+      // If OpenALPR has potential matches
+      if (
+        results &&
+        results.length > 0 &&
+        results[0]["candidates"].length > 0
+      ) {
+        cars = [];
+        results[0]["candidates"].forEach((car) => {
+          if (car["confidence"] > MIN_CAR_CONFIDENCE) {
+            console.log("Adding:", car);
+            let carEntry = {};
+            carEntry["plate"] = car["plate"];
+            carEntry["color"] = results[0]["vehicle"]["color"][0]["name"];
+            carEntry["make"] = results[0]["vehicle"]["make"][0]["name"];
+            carEntry["model"] = results[0]["vehicle"]["make_model"][0]["name"];
+            cars.push(carEntry);
+          }
+        });
+      } else {
+        console.log("No results from OpenALPR");
+      }
+      await processResults(cars);
+    })
+    .catch((err) => {
+      console.err("Upload failed:", err);
+      processing_image = false;
+    });
+};
 
 /**
  *  Attempt to find matches in database for cars identified by OpenALPR results
  *  @param cars: A list of potential car objects as outlined in OpenALPR API
- *  @returns void
- */ 
-async function processResults(cars) {
-	if (cars === undefined || cars.length == 0) {
-		console.log('No car detected');
-		processing_image = false;
-		return;
-	}
+ */
+const processResults = async (cars) => {
+  if (!cars || cars.length === 0) {
+    console.log("No car detected");
+    processing_image = false;
+    return;
+  }
 
-	var known_cars_array = [];
-	console.log('Possibilities:', cars);
-	var ref = database.ref("Member");
+  let known_cars_array = [];
+  console.log("Possibilities:", cars);
+  const ref = database.ref("Member");
 
-	// Pull all recognized cars from Firebase
-	await ref.once("value", function(snapshot) {
-		snapshot.forEach(function(data) {
-			known_cars_array.push(data.val());
-		});
-	}, function (errorObject) {
-		console.log("ERROR: Firebase" + errorObject.code);
-	});
+  // Pull all recognized cars from Firebase
+  await ref.once(
+    "value",
+    (snapshot) => {
+      snapshot.forEach((data) => {
+        known_cars_array.push(data.val());
+      });
+    },
+    (errorObject) => {
+      console.err("ERROR: Firebase" + errorObject.code);
+    }
+  );
 
-	// Compare OpenALPR results against Firebase entries
-	var found = known_cars_array.find((knownCar) => {
-		return cars.find((carGuess) => {
-			return knownCar['plate'] === carGuess['plate'];
-		});
-	});
+  // Compare OpenALPR results against Firebase entries
+  const found = known_cars_array.find((knownCar) =>
+    cars.find((carGuess) => knownCar["plate"] === carGuess["plate"])
+  );
 
-	// Check for car match
-	if (found) {
-		console.log('Plate match! ' + JSON.stringify(found));
-		await tryOpenDoor();
-	} else {
-		console.log('No match!');
+  // Check for car match
+  if (found) {
+    console.log("Plate match:", JSON.stringify(found));
+    await tryOpenDoor();
+  } else {
+    console.log("No match!");
 
-		var unknown_ref = database.ref("LoadImage");
-		var pending_cars = [];
+    const unknown_ref = database.ref("LoadImage");
+    let pending_cars = [];
 
-		// Push unknown car license plate to Firebase
-		await unknown_ref.once("value", function(snapshot) {
-			snapshot.forEach(function(data) {
-				pending_cars.push(data.val());
-			});
-		}, function (errorObject) {
-			console.log("ERROR: Firebase" + errorObject.code);
-		});
+    // Push unknown car license plate to Firebase
+    await unknown_ref.once(
+      "value",
+      (snapshot) => {
+        snapshot.forEach((data) => {
+          pending_cars.push(data.val());
+        });
+      },
+      (errorObject) => {
+        console.err("ERROR: Firebase", errorObject.code);
+      }
+    );
 
-		var alreadyUploaded = pending_cars.find((pendingCar) => {
-			return pendingCar['plate'] === cars[0]['plate'];
-		});
+    const alreadyUploaded = pending_cars.find(
+      (pendingCar) => pendingCar["plate"] === cars[0]["plate"]
+    );
 
-		if (!alreadyUploaded) {
-			// Upload image of unrecognized car to Firebase database
-			try {
-				bucket.upload('latest.jpg', { destination: cars[0]['plate'] });
-			} catch (error) {
-				console.log('Failed image upload, aborting.');
-				processing_image = false;
-				return;
-			}
+    if (!alreadyUploaded) {
+      // Upload image of unrecognized car to Firebase database
+      try {
+        bucket.upload("latest.jpg", { destination: cars[0]["plate"] });
+      } catch (_error) {
+        console.err("Failed image upload.");
+        processing_image = false;
+        return;
+      }
 
-			unknown_ref.push(cars[0]);
-			console.log('Uploaded unrecognized car info!');
-		} else {
-			console.log('Unrecognized car already uploaded.');
-		}
-		// NOTE: Listeners for image uploads will trigger the Android app to notify user
-		// of unrecognized vehicle and allow he/she to open gate from the app by hitting
-		// the webserver with a valid put request
+      unknown_ref.push(cars[0]);
+      console.log("Uploaded unrecognized car info!");
+    } else {
+      console.log("Unrecognized car already uploaded.");
+    }
+    // NOTE: Listeners for image uploads will trigger the Android app to notify user
+    // of unrecognized vehicle and allow he/she to open gate from the app by hitting
+    // the webserver with a valid put request
 
-		processing_image = false;
-	}
-}
+    processing_image = false;
+  }
+};
 
 /**
  *  Attempts to open the garage door, if it is not being opened or already open
- *  @returns void
- */ 
-async function tryOpenDoor() {
-	console.log("Opening door...");
+ */
+const tryOpenDoor = async () => {
+  console.log("Opening door...");
 
-	if (door_busy || door_open) {
-		console.log("Already busy or open, aborting.");
-		return;
-	}
+  if (door_busy || door_open) {
+    console.err("Door already busy or open.");
+    return;
+  }
 
-	door_busy = true;
-	var openFunction = setInterval(moveServo, 100);
+  door_busy = true;
+  const openFunction = setInterval(moveServo, 100);
 
-	// Inject a function to stop opening the door and reset variables when "open"
-	function stopFunction() {
-		clearInterval(openFunction);
-		motor.servoWrite(0);
-		door_open = true;
-		door_busy = false;
-		console.log("Open!");
-	}
+  const stopFunction = () => {
+    clearInterval(openFunction);
+    motor.servoWrite(0);
+    door_open = true;
+    door_busy = false;
+    console.log("Open!");
+  };
 
-	// Injected function to incrementally move servo until "open" threshold is reached
-	function moveServo() {
-		if (current_pw >= OPEN_PW) {
-			motor.servoWrite(current_pw);
-			current_pw -= increment;
-		} else {
-			current_pw = OPEN_PW;
-			stopFunction();
-		}
-	}
+  const moveServo = () => {
+    if (current_pw >= OPEN_PW) {
+      motor.servoWrite(current_pw);
+      current_pw -= increment;
+    } else {
+      current_pw = OPEN_PW;
+      stopFunction();
+    }
+  };
 
-	await new Promise(() => setTimeout(() => { closeDoor() }, SECONDS_OPEN * 1000));
-}
+  await new Promise(() =>
+    setTimeout(() => {
+      closeDoor();
+    }, SECONDS_OPEN * 1000)
+  );
+};
 
 /**
  *  Attempts to close the garage door, if it is not closing or already closed
- *  @returns void
- */ 
-async function closeDoor() {
-	console.log("Closing door...");
+ */
+const closeDoor = async () => {
+  console.log("Closing door...");
 
-	if (door_busy || !door_open) {
-		console.log("Already busy or closed, aborting.");
-		return;
-	}
+  if (door_busy || !door_open) {
+    console.err("Door already busy or closed.");
+    return;
+  }
 
-	door_busy = true;
-	var closeFunction = setInterval(moveServo, 100);
+  door_busy = true;
+  const closeFunction = setInterval(moveServo, 100);
 
-	// Stop closing
-	function stopFunction() {
-		clearInterval(closeFunction);
-		motor.servoWrite(0);
-		door_open = false;
-		door_busy = false;
-		console.log("Closed!");
-		processing_image = false;
-	}
+  const stopFunction = () => {
+    clearInterval(closeFunction);
+    motor.servoWrite(0);
+    door_open = false;
+    door_busy = false;
+    console.log("Closed!");
+    processing_image = false;
+  };
 
-	// Repeat servo move until closed
-	function moveServo() {
-		if (current_pw <= CLOSED_PW) {
-			motor.servoWrite(current_pw);
-			current_pw += increment;
-		} else {
-			current_pw = CLOSED_PW;
-			stopFunction();
-		}
-	}
-}
-
-module.exports = {
-	/**
-	 *  Allows the main app to send the most recently taken picture to ALPR and start open logic
-	 *  @returns void
-	 */ 
-	async sendPicToCloud() {
-		if (processing_image) return;
-		console.log("Motion detected, taking picture...");
-		processing_image = true;
-		await myCamera.snap()
-		.then(async (result) => {
-			console.log("Taken!");
-		})
-		.catch((error) => {
-			console.log("ERROR: Camera", error);
-		});
-		// Successfully taken, carry on to sending picture
-		sendPicture().catch((error) => {
-			console.log("ERROR: OpenALPR", error);
-		});
-	},
-	/**
-	 *  Reset servo position on server start
-	 *  @returns void
-	 */ 
-	async initializeServo() {
-		motor.servoWrite(CLOSED_PW);
-		current_pw = CLOSED_PW;
-		setTimeout(() => { motor.servoWrite(0) }, 3000);
-	}
+  // Repeat servo move until closed
+  const moveServo = () => {
+    if (current_pw <= CLOSED_PW) {
+      motor.servoWrite(current_pw);
+      current_pw += increment;
+    } else {
+      current_pw = CLOSED_PW;
+      stopFunction();
+    }
+  };
 };
+
+/**
+ *  Allows the main app to send the most recently taken picture to ALPR and start open logic
+ */
+const sendPicToCloud = async () => {
+  if (processing_image) return;
+  console.log("Taking picture...");
+
+  processing_image = true;
+  await myCamera
+    .snap()
+    .then(async (result) => {
+      console.log("Taken!");
+    })
+    .catch((error) => {
+      console.err("ERROR: Camera", error);
+    });
+  // Successfully taken, carry on to sending picture
+  sendPicture().catch((error) => {
+    console.err("ERROR: OpenALPR", error);
+  });
+};
+
+/**
+ *  Reset servo position on server start
+ */
+const initializeServo = async () => {
+  motor.servoWrite(CLOSED_PW);
+  current_pw = CLOSED_PW;
+  setTimeout(() => {
+    motor.servoWrite(0);
+  }, 3000);
+};
+
+export { sendPicToCloud, initializeServo };
